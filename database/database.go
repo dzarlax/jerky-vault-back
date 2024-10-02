75e67992d1a4fe0575ac7f6301e3ecf888c8e2f3
Dzarlax <me@dzarlax.dev>
2024-10-02 15:17:42 +0200

    Migrate to postgre

--- database/database.go
+++ database/database.go
@@ -1,10 +1,11 @@
 package database
 
 import (
-    "fmt"
+    //"fmt"
     "log"
     "os"
-    "gorm.io/driver/mysql"
+    // "gorm.io/driver/mysql"
+    "gorm.io/driver/postgres"
     "gorm.io/gorm"
     "mobile-backend-go/models"
 )
@@ -13,15 +14,23 @@ var DB *gorm.DB
 
 // ConnectDatabase устанавливает подключение к базе данных и выполняет миграции
 func ConnectDatabase() {
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_NAME"),
-    )
+    // dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+    //     os.Getenv("DB_USER"),
+    //     os.Getenv("DB_PASSWORD"),
+    //     os.Getenv("DB_HOST"),
+    //     os.Getenv("DB_PORT"),
+    //     os.Getenv("DB_NAME"),
+    // )
+    dsn := os.Getenv("DATABASE_URL")
+    if dsn == "" {
+        log.Fatal("Переменная окружения DATABASE_URL не задана")
+    }
 
-    database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    // database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    // if err != nil {
+    //     log.Fatal("Не удалось подключиться к базе данных: ", err)
+    // }
+    database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Не удалось подключиться к базе данных: ", err)
     }
--- go.mod
+++ go.mod
@@ -6,16 +6,23 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/jackc/pgx/v5 v5.5.5
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.3
 	golang.org/x/crypto v0.26.0
-	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.11
 )
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/bytedance/sonic v1.12.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
@@ -30,7 +37,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
